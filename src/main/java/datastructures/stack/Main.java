dc8c9450d2506719468629fb4dee16c4eec23671
nijatburjiyev <nijatburjiyev@gmail.com>
2024-10-24 16:32:19 -0700

    Stack Constructor, printStack(), getTop(), getHeight()

---
+++ src/main/java/datastructures/stack/Main.java
@@ -0,0 +1,11 @@
+package datastructures.stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack stack = new Stack(4);
+        stack.getTop();
+        stack.getHeight();
+        stack.printStack();
+    }
+}
